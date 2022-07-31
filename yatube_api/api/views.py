915a112e7dea15f6b1b9f76597a9769e822d0944
VladBorisof <vl-borisof@yandex.ru>
2022-07-31 18:54:21 +0300

    add serializers for follow

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,10 +2,9 @@ from rest_framework import filters, mixins, permissions, viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
 
-from .permissions import AuthorPermission
-from .serializers import (
-    CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer
-)
+from api.permissions import AuthorPermission
+from api.serializers import (CommentSerializer, FollowSerializer,
+                             GroupSerializer, PostSerializer)
 from posts.models import Group, Post, User
 
 
